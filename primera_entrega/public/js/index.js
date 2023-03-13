4ad13b2bf515b423ff51dfc5164369f5b8895fc2
luchopesce <luchopesce96@gmail.com>
2023-03-13 20:15:05 -0300

    fix products

--- primera_entrega/public/js/index.js
+++ primera_entrega/public/js/index.js
@@ -8,7 +8,7 @@ socket.on("list-products", (data) => {
 
   for (const el of data) {
     const li = document.createElement("li");
-    li.innerText = `${el.title}: ${el.price}`;
+    li.innerText = `${el.title}: ${el.price}, ID: ${el.id}, CODE: ${el.code}, STATUS:${el.status}, STOCK:${el.stock}`;
     listProducts.appendChild(li);
   }
 });
